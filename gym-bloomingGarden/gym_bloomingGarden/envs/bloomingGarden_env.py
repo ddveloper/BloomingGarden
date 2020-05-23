128c293637d96c04e94f2d1df52c2ff8070c70a4
ddveloper <44758313+ddveloper@users.noreply.github.com>
2020-05-23 01:33:02 -0700

    add hint (valid start node -> valid end node)

--- gym-bloomingGarden/gym_bloomingGarden/envs/bloomingGarden_env.py
+++ gym-bloomingGarden/gym_bloomingGarden/envs/bloomingGarden_env.py
@@ -15,17 +15,19 @@ class bloomingGardenEnv(gym.Env):
     self.coming = [3, 3, 3]
     self.reset()
     self.action_space = spaces.Tuple((spaces.Discrete(9), spaces.Discrete(9), spaces.Discrete(9), spaces.Discrete(9)))
-    #self.observation_space = spaces.
+    self.observation_space = spaces.Box(0.0, 1.0, (9,9))
 
   def step(self, action):
     reward = 0
     done = np.count_nonzero(self.brd == 3) == 0
     if done:
-      return (self.brd, self.coming, self.score), reward, done, {}
+      brd_state = np.concatenate((self.brd.flatten(), np.array(self.coming)), axis=0)
+      return (brd_state, self.score), reward, done, {self._movable_flower()}
 
     if not self._is_valid_action(action): # action is invalid
       reward = 0
-      return (self.brd, self.coming, self.score), reward, done, {}
+      brd_state = np.concatenate((self.brd.flatten(), np.array(self.coming)), axis=0)
+      return (brd_state, self.score), reward, done, {self._movable_flower()}
 
     # take the action and calculate score
     sr, sc, tr, tc = action
@@ -52,11 +54,42 @@ class bloomingGardenEnv(gym.Env):
     # update comings
     self._update_coming()
 
-    return (self.brd, self.coming, self.score), reward, done, {}
+    brd_state = np.concatenate((self.brd.flatten(), np.array(self.coming)), axis=0)
+    return (brd_state, self.score), reward, done, {self._movable_flower()}
+  
+  def _movable_flower(self):
+    ''' return one movable flower coordinates
+    '''
+    r0, c0 = np.where(self.brd != 3)
+    r1, c1 = np.where(self.brd == 3)
+    if len(r0) == 81: return (0, 0, 0, 1) # not movable at all, return dummy value
+    if len(r0) < 40: # to improve efficiency, when flowers are sparse
+      while True:
+        k = np.random.randint(0,len(r0))
+        r, c = r0[k], c0[k]
+        if (r > 0 and self.brd[r-1][c] == 3) or (r < 8 and self.brd[r+1][c] == 3) \
+          or (c > 0 and self.brd[r][c-1] == 3) or (c < 8 and self.brd[r][c+1] == 3): 
+          k = np.random.randint(0,len(r1))
+          return (r, c, r1[k], c1[k])
+    else: # when blanks are sparse
+      while True:
+        k = np.random.randint(0,len(r1))
+        r, c = r1[k], c1[k]
+        if (r > 0 and self.brd[r-1][c] != 3): 
+          k = np.random.randint(0,len(r1)); return (r-1, c, r1[k], c1[k])
+        if (r < 8 and self.brd[r+1][c] != 3): 
+          k = np.random.randint(0,len(r1)); return (r+1, c, r1[k], c1[k])
+        if (c > 0 and self.brd[r][c-1] != 3): 
+          k = np.random.randint(0,len(r1)); return (r, c-1, r1[k], c1[k])
+        if (c < 8 and self.brd[r][c+1] != 3): 
+          k = np.random.randint(0,len(r1)); return (r, c+1, r1[k], c1[k])
+    return (0, 0, 0, 1)
 
   def reset(self):
     ''' flowers [0-pink, 1-green, 2-yellow, 3-empty, 4-blue
                  5-purple, 6-white, 7-red]
+        reset game
+        return board status & coming flowers
     '''
     self.score = 0
     self.brd = np.full((9, 9), 3, dtype=np.int)
@@ -65,8 +98,11 @@ class bloomingGardenEnv(gym.Env):
       flower = np.random.choice(self.flowers)
       self.brd[position//9][position%9] = flower
     self._update_coming()
+    return np.concatenate((self.brd.flatten(), np.array(self.coming)), axis=0)
 
   def render(self, mode='human'):
+    ''' formatted board status, score and coming flowers
+    '''
     brd = '\n     0 1 2 3 4 5 6 7 8\n'
     brd += '     ~ ~ ~ ~ ~ ~ ~ ~ ~ \n'
 
@@ -99,25 +135,28 @@ class bloomingGardenEnv(gym.Env):
     return False
 
   def _update_coming(self):
-    self.coming = []
-    for i in range(3): self.coming.append(np.random.choice(self.flowers))
+    ''' randomly choose 3 flowers into self.coming[]
+    '''
+    for i in range(3): self.coming[i] = (np.random.choice(self.flowers))
 
   def _update_brd(self, r, c):
     gain = 0
     val = self.brd[r][c]
-    # horizontal update
+    # check possible updates following "vertical, horizontal, \ diagnose, / diagnose"
     for dr1,dc1,dr2,dc2 in [(-1,0,1,0), (0,-1,0,1), (-1,-1,1,1),(-1,1,1,-1)]:
       lcnt = 0
       nr, nc = r+dr1, c+dc1
+      # look backwards
       while nr >= 0 and nr < 9 and nc >=0 and nc < 9 \
         and self.brd[nr][nc] == val:
         nr, nc = nr+dr1, nc+dc1; lcnt += 1
       rcnt = 0
       nr, nc = r+dr2, c+dc2
+      # look forwards
       while nr >= 0 and nr < 9 and nc >=0 and nc < 9 \
         and self.brd[nr][nc] == val:
         nr, nc = nr+dr2, nc+dc2; rcnt += 1
-      if lcnt + rcnt + 1 >= 5: # can be clean
+      if lcnt + rcnt + 1 >= 5: # when more than 5 continuous same flowers, can be clean
         for i in range(1, lcnt+1): self.brd[r+i*dr1][c+i*dc1] = 3
         for i in range(1, rcnt+1): self.brd[r+i*dr2][c+i*dc2] = 3
         gain = [5,8,13,20,28,13][lcnt+rcnt+1-5]
--- test_evn.py
+++ test_evn.py
@@ -5,7 +5,10 @@ import numpy as np
 env = gym.make("bloomingGarden-v0")
 env.reset()
 done = False
+movable = (0, 0, 0, 1)
 while not done:
     env.render()
-    action = env.action_space.sample()
-    ns, rw, done, _ = env.step(action)
+    action_rand = env.action_space.sample()
+    action = (movable[0], movable[1], action_rand[2], action_rand[3])
+    ns, rw, done, mv = env.step(action)
+    movable = mv.pop()
